3d4cebfb4e7e2182f4d616dffe72312ceee152e6
DiePutze5853 <83286522+DiePutze5853@users.noreply.github.com>
2021-12-22 14:59:26 +0100

    fix
    EVM chain ID error

--- iscp/deps/wasp-cli/docker-entrypoint.sh
+++ iscp/deps/wasp-cli/docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [[ -z "$(./wasp-cli -c ./wasp-cli.json chain info | grep "Test EVM")" ]]; the
     ./wasp-cli request-funds -w
     ./wasp-cli chain deploy --committee=0 --quorum=1 --chain=test-evm --description="Test EVM" -w
     ./wasp-cli chain deposit IOTA:10000 -w 
-    ./wasp-cli chain evm deploy -a test-evm --alloc 0x9C0B7a35B957F23b83677C6429F1F393027A9E62:1000000000000000000000000 -w
+    ./wasp-cli chain evm deploy -a test-evm --alloc 0x9C0B7a35B957F23b83677C6429F1F393027A9E62:1000000000000000000000000 --chainid 1075 -w
 fi
 
 ./wasp-cli chain evm jsonrpc --chainid 1075
